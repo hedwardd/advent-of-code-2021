361e764d4cac2c37fda8707bb315955e8b42fdf8
Heath Daniel <mr.heath.daniel@gmail.com>
2021-12-18 18:28:18 -0500

    Day 4 part 1

--- day4.ts
+++ day4.ts
@@ -1,6 +1,5 @@
-import input from './day3input';
-
-const inputStringsFromInput = (input: string): string[] => input.split('\n');
+import input from './day4input';
+// import input from './day4SampleInput';
 
 /* Approaches
 
@@ -10,11 +9,113 @@ const inputStringsFromInput = (input: string): string[] => input.split('\n');
   4. Store the values called so far, check each row and column to see if all values have been stored so far
 */
 
+type Board = number[][];
+
 // Parse the input
+const splitInput = splitInputByBlankLines(input);
+
 // Separate the numbers to be called and the boards
-// Create an object to store numbers called so far
-// Go through the numbers to be called and process each board
-// Check each row and column to see if every value is in called numbers map
-// Once winning board is found, get sum of numbers that weren't called
-// Multiply sum by last number called
+const callNumbers = splitInput[0].split(',').map(number => parseInt(number, 10));
+const boardStrings = splitInput.slice(1);
+
+// Convert the board strings to 2D arrays
+const boards: Board[] = boardStrings.map(boardString => {
+  return boardString.split('\n').map(row => row.trim().split(/\s+/).map(number => parseInt(number)));
+});
+
+// Create an set to store numbers called so far
+const numbersCalled = new Set<number>();
+
+function getWinningScore(): number {
+  // Go through the numbers to be called and process each board
+  // console.log('Processing boards...');
+  // console.log(`There are ${boards.length} boards`);
+  // console.log(`There are ${callNumbers.length} numbers to be called`);
+  // console.log(`The boards are:`);
+  // console.log(boards);
+  for (let i = 0; i < callNumbers.length; i++) {
+    const currentNumberCalled = callNumbers[i];
+    // console.log(`Processing number ${currentNumberCalled}`);
+    numbersCalled.add(currentNumberCalled);
+    // console.log(`Numbers called so far: ${[...numbersCalled].join(', ')}`);
+    const winningBoard = checkForWinningBoard(boards, numbersCalled);
+    
+    // Once winning board is found, get sum of numbers that weren't called
+    if (winningBoard !== null) {
+      console.log(`The winning board is: `);
+      console.table(winningBoard);
+      const sumOfNumbersNotCalled = getSumOfNumbersNotCalled(winningBoard, numbersCalled);
+      console.log(`The sum of numbers not called is ${sumOfNumbersNotCalled}`);
+      const winningScore = calculateWinningScore(sumOfNumbersNotCalled, currentNumberCalled);
+      return winningScore;
+    }
+  }
+  return 0;
+}
+
+console.log(getWinningScore());
+  
+
+
+function splitInputByBlankLines(input: string): string[] {
+  return input.split('\n\n');
+}
+
+function checkForWinningBoard(boards: Board[], numbersCalled: Set<number>): Board | null {
+  for (let board of boards) {
+    const winningBoard = checkBoardForWinningBoard(board, numbersCalled);
+    if (winningBoard !== null) {
+      return winningBoard;
+    }
+  }
+  return null;
+}
+
+function checkBoardForWinningBoard(board: Board, numbersCalled: Set<number>): Board | null {
+  // Check rows
+  // If every number in a row has been called, return the board
+  // console.log('Checking board: ');
+  // console.table(board);
+  for (let row of board) {
+    const allNumbersCalled = row.every(number => numbersCalled.has(number));
+    if (allNumbersCalled) {
+      console.log(`All numbers in row ${row} have been called`);
+      return board;
+    }
+  }
+  // Check columns
+  // If every number in a column has been called, return the board
+  for (let c = 0; c < board[0].length; c++) {
+    for (let r = 0; r < board.length; r++) {
+      const number = board[r][c];
+      // console.log(`Checking column ${c}, row ${r} for number ${number}`);
+      const numberCalled = numbersCalled.has(number);
+      if (!numberCalled) {
+        break;
+      }
+      if (r === board[0].length - 1) {
+        console.log(`All numbers in column ${c} have been called`);
+        return board;
+      }
+    }
+  }
+  return null;
+}
+
+function getSumOfNumbersNotCalled(winningBoard: Board, numbersCalled: Set<number>): number {
+  let sumOfNumbersNotCalled = 0;
+  for (let row of winningBoard) {
+    for (let number of row) {
+      if (!numbersCalled.has(number)) {
+        sumOfNumbersNotCalled += number;
+      }
+    }
+  }
+  return sumOfNumbersNotCalled;
+}
+
+function calculateWinningScore(sumOfNumbersNotCalled: number, currentNumberCalled: number) {
+  // Multiply sum by last number called
+  return sumOfNumbersNotCalled * currentNumberCalled;
+}
 
