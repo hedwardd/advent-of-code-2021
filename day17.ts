8cc49b148620b1b590d0b7ca449bfb1e86263fd2
Heath Daniel <mr.heath.daniel@gmail.com>
2022-01-07 19:44:28 -0500

    Day 17 Part 1

---
+++ day17.ts
@@ -0,0 +1,85 @@
+import input from './day17input';
+// import input from './day17SampleInput';
+
+type Position = [number, number];
+type Velocity = [number, number];
+type State = [Position, Velocity];
+type Minimum = number;
+type Maximum = number;
+type Range = [Minimum, Maximum];
+type TargetArea = [xRange: Range, yRange: Range];
+
+const STARTING_POINT = [0, 0];
+
+// example input: "target area: x=20..30, y=-10..-5"
+function parseRawInputToTargetArea(input: string): TargetArea {
+  const ranges = input.split(': ');
+  const [xRangeString, yRangeString] = ranges[1].split(', ');
+  const xRange = parseRange(xRangeString);
+  const yRange = parseRange(yRangeString);
+
+  return [xRange, yRange];
+}
+
+function parseRange(rangeString: string): Range {
+  return rangeString.slice(2).split('..').map(Number) as Range;
+}
+
+function getNextState([[xPosition, yPosition], [xVelocity, yVelocity]]: State): State {
+  const nextXPosition = xPosition + xVelocity;
+  const nextYPosition = yPosition + yVelocity;
+  // xVelocity approaches 0, yVelocity constantly decreases
+  const nextXVelocity =
+    xVelocity > 0
+      ? xVelocity - 1
+      : xVelocity < 0
+        ? xVelocity + 1
+        : xVelocity;
+  const nextYVelocity = yVelocity - 1;
+  return [[nextXPosition, nextYPosition], [nextXVelocity, nextYVelocity]];
+}
+
+function getMaxAltitudeFromStartingVelocity(startingVelocity: Velocity, targetArea: TargetArea): number | undefined {
+  const [xRange, yRange] = targetArea;
+  const [minX, maxX] = xRange;
+  const [minY, maxY] = yRange;
+  let currentPosition: Position = [0, 0];
+  let [currentXPosition, currentYPosition] = currentPosition;
+  let currentState: State = [currentPosition, startingVelocity];
+  let maxAltitude = 0;
+  let hitsTargetArea = false;
+  while (currentXPosition <= maxX && currentYPosition >= minY) {
+    currentState = getNextState(currentState);
+    currentPosition = currentState[0];
+    [currentXPosition, currentYPosition] = currentPosition;
+    maxAltitude = Math.max(currentYPosition, maxAltitude);
+    if (currentXPosition >= minX && currentXPosition <= maxX && currentYPosition <= maxY && currentYPosition >= minY) {
+      hitsTargetArea = true;
+    }
+  }
+  return hitsTargetArea ? maxAltitude : undefined;
+}
+
+/* Approaches:
+  1. Get an initial estimate, try different trajectories, try to maximize for altitude
+  2. Work backwards to create functions that will go through, find the one with max alt
+  3. Brute force: find one that works, try every possible value +/- 1000
+  4. Try different trajectories, updating x or y based on whether overshooting/undershooting
+*/
+
+const targetArea = parseRawInputToTargetArea(input);
+// console.log(targetArea);
+
+let maxAltitude = -Number.MAX_VALUE;
+
+// Bruce force
+for (let xVelocity = 0; xVelocity <= targetArea[0][1]; xVelocity++) {
+  for (let yVelocity = 0; yVelocity <= 1000; yVelocity++) {
+    const altitude = getMaxAltitudeFromStartingVelocity([xVelocity, yVelocity], targetArea);
+    if (altitude) {
+      maxAltitude = Math.max(maxAltitude, altitude);
+    }
+  }
+}
+
+console.log(maxAltitude)
